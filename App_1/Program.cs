548e75cd7646ca51326defc2e91a07b65f6a041c
Nikita Belov <lolvvto@yandex.ru>
2022-11-26 15:24:21 +0700

    Add App_1

--- App_1/Program.cs
+++ App_1/Program.cs
@@ -41,21 +41,21 @@ void PrintArray(int[,] array)
 
 void SortDescending(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
         }
-      }
     }
-  }
 }
 
 
